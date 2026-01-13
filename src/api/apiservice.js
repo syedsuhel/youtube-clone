596aa86c0e17051713e5ad96ecce51b6c2632ec3
Syed Suhel M <syedsuhel2624@gmail.com>
2026-01-13 10:48:34 +0530

    bootstrap cdn added

--- src/api/apiservice.js
+++ src/api/apiservice.js
@@ -4,5 +4,4 @@ const baseApi = "https://www.googleapis.com/youtube/v3";
 
 export const searchApi=(searchStr)=>{
     return axios.get(`${baseApi}/search?part=snippet&q=${searchStr}&key=${import.meta.env.VITE_YOUTUBE_DATA_API_KEY}`)
-
 }
