19eaa4bb50f659d9606382fd7e8a00c762f34af6
Syed Suhel M <syedsuhel2624@gmail.com>
2026-01-13 10:33:17 +0530

    set a api configuration

---
+++ src/api/apiservice.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseApi = "https://www.googleapis.com/youtube/v3";
+
+export const searchApi=(searchStr)=>{
+    return axios.get(`${baseApi}/search?part=snippet&q=${searchStr}&key=${import.meta.env.VITE_YOUTUBE_DATA_API_KEY}`)
+
+}
