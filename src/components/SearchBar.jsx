68cebdd7104d0b387615c78e3158c7d55145cf83
syedsuhel <syedsuhel2624@gamil.com>
2026-01-18 14:19:15 +0530

    add search bar input function

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,28 +1,28 @@
-const SearchBar = () => {
+import { useState } from "react";
+
+const SearchBar = (handleSearch) => {
+  const[search,setSearch]=useState ("");
   return (
-    <form className="d-flex" style={barSizing} role="search">
+    <form className="d-flex" style={barSizing} onSubmit={(e)=>{
+      e.preventDefault();
+      handleSearch(search)
+    }}>
+      {search}
       <input
         className="border border-secondary border-end rounded-start-pill ps-3 "
         type="text"
         placeholder="Search"
-        aria-label="Search"
+        aria-label="Recipient’s usernameSearch"
         style={{ ...height, ...srcSizing }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button
         className="btn border-secondary rounded-0 border-start-0 rounded-end-pill d-flex justify-content-center align-items-center bg-white"
         type="submit"
         style={height}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          class="bi bi-search"
-          viewBox="0 0 16 16"
-        >
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
-        </svg>
+        <i className="fa-solid fa-magnifying-glass"></i>
       </button>
     </form>
   );
@@ -35,7 +35,7 @@ const barSizing = {
   marginRight: "8%",
 };
 const srcSizing = {
-    maxWidth:"20rem",
+  maxWidth: "20rem",
   width: "75vw",
 };
 export default SearchBar;
